5875143f3fc0086d93ef9b42b47e7af77c4d308f
unknown <henriquearthur.dev@gmail.com>
2025-11-13 19:44:19 -0300

    upando o initializr pra reiniciar o computador sem perder

---
+++ Conecta/Conecta/src/test/java/br/edu/EtecZonaLeste/Conecta/ConectaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.EtecZonaLeste.Conecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
